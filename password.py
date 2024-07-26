f3fcfe629aedf7f1caa325425bbaeade628c9fdb
joseph-akintunde <57961001+joseph-akintunde@users.noreply.github.com>
2024-07-26 06:07:36 -0500

    Update password.py

--- password.py
+++ password.py
@@ -9,22 +9,6 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-"""
-password = ""
-for i in range(1,nr_letters+1):
-  password += random.choice(letters)
-for j in range(1, nr_symbols+1):
-  password += random.choice(symbols)
-for k in range(1,nr_numbers+1):
-  password += random.choice(numbers)
-print(password)
-"""
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 password = ""
 for i in range(1,nr_letters+1):
   password += random.choice(letters)
